90deb2fcdf5b85afd913cd7d6a4fddebb1d8dfd7
pato0916 <patricio.pizarro32@gmail.com>
2024-10-23 01:07:31 -0300

    movimiento del objeto

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,51 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private float horizontalMove;
-    private float verticalMove;
+    public float horizontalMove;
+    public float verticalMove;
     public CharacterController player;
 
-    public float playerSpeed = 15;
-    private Vector3 movePlayer;
-
-    public Camera mainCamera;
-    private Vector3 camForward;
-    private Vector3 camRight;
-
-    private Vector3 playerInput;
-    // Start is called before the first frame update
+    public float playerSpeed;
     void Start()
     {
         player = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        horizontalMove = Input.GetAxis("Horizontal");
+        horizontalMove =  Input.GetAxis("Horizontal");
         verticalMove = Input.GetAxis("Vertical");
-
-        playerInput = new Vector3(horizontalMove,0,verticalMove);
-        playerInput = Vector3.ClampMagnitude(playerInput, 1);
-
-        camDirection();
-
-        movePlayer = playerInput.x*camRight+playerInput.z*camForward;
-
-        player.transform.LookAt(player.transform.position + movePlayer);
-
-        player.Move(movePlayer*playerSpeed* Time.deltaTime);
     }
 
-    private void FixedUpdate(){
-        
- }
-    void camDirection(){
-        camForward = mainCamera.transform.forward;
-        camRight = mainCamera.transform.right;
-        camForward.y = 0;
-        camRight.y=0;
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
+    private void FixedUpdate()
+    {
+        player.Move(new Vector3(horizontalMove,0,verticalMove) * playerSpeed * Time.deltaTime);
     }
+    
+    
 }
